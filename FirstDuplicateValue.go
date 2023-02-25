bf7f4bd6c77d99a27a7b37bb1a4e8e6525ab38ed
altairsantos <altairsantosvix@gmail.com>
2023-02-24 22:50:07 -0500

    first one

---
+++ FirstDuplicateValue.go
@@ -0,0 +1,23 @@
+package main
+
+// O(n) time
+// O(1) space
+func FirstDuplicateValue(arr []int) int {
+for _,  value := range arr {
+
+    absValue := absV(value)
+    if arr[absValue-1] < 0 {
+            return absValue
+    } 
+    arr[absValue-1] *= -1
+}
+	return -1
+}
+
+func absV( n int ) int {
+    if n < 0 {
+        return -n
+    }
+     return n
+}
+
