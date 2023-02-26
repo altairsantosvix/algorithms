56159a2240cdae9913d20d13ae61dcd660b8436b
altairsantos <altairsantosvix@gmail.com>
2023-02-26 10:24:53 -0500

    add test case

--- FirstDuplicateValue/FirstDuplicateValue.go
+++ FirstDuplicateValue/FirstDuplicateValue.go
@@ -7,15 +7,15 @@ import (
 // O(n) time
 // O(1) space
 func FirstDuplicateValue(arr []int) int {
-for _,  value := range arr {
+    for _,  value := range arr {
 
-    absValue := absV(value)
-    if arr[absValue-1] < 0 {
-            return absValue
-    } 
-    arr[absValue-1] *= -1
-}
-	return -1
+        absValue := absV(value)
+        if arr[absValue-1] < 0 {
+                return absValue
+        } 
+        arr[absValue-1] *= -1
+    }
+        return -1
 }
 
 func absV( n int ) int {
@@ -24,14 +24,3 @@ func absV( n int ) int {
     }
      return n
 }
-
-func main() {
-
-    arr := []int{1, 2, 3, 4, 4,5}
-
-    n := FirstDuplicateValue( arr)
-
-    fmt.Printf(" First duplicate value in array is %d \n",  n)
-
-}
-
